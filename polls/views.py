bbfb793e16bf4983c85a120a1d69f186e9a84d11
paullarasales <rkise2131@gmail.com>
2025-01-14 23:03:12 +0800

    done defining the mode;

--- polls/views.py
+++ polls/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def fuck(request):
+    return HttpResponse("hoping this capstone will not fuck my year..")
