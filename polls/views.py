c1779dfecc9f9569bbdb429760df9c826bd4d13c
paullarasales <rkise2131@gmail.com>
2025-01-17 19:21:44 +0800

    added DetailView & ResultsView

--- polls/views.py
+++ polls/views.py
@@ -13,6 +13,13 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         return Question.objects.order_by("-pub_date")[:5]
     
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+class ResultsView(generic.ResultsView):
+    model = Question
+    template_name = "polls/results.html"
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
