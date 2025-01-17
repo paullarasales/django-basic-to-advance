2e76cd1da801d719a57410d8db64aff100a484f0
paullarasales <rkise2131@gmail.com>
2025-01-17 21:23:01 +0800

    done creating test cases

--- polls/views.py
+++ polls/views.py
@@ -17,7 +17,7 @@ class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
 
-class ResultsView(generic.ResultsView):
+class ResultsView(generic.DetailView):
     model = Question
     template_name = "polls/results.html"
 
